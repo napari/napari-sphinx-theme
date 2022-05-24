49fd89c8e4314e910372bd05cf990b2591292ef4
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-05-24 16:43:22 -0700

    Update to v0.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021 napari Community"
 author = "napari Community"
 
 
-release = '0.1.1'
+release = '0.1.2'
 version = release.replace("dev0", "")
 
 # -- General configuration ---------------------------------------------------
