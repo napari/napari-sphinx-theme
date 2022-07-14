23331b4e87b5522f89e7e3220bc67bdc49b26947
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-07-14 15:28:35 -0700

    Fix admonition dropdown buttons (#118)

---
+++ src/napari_sphinx_theme/assets/components/icons/PlusIcon.tsx
@@ -0,0 +1,22 @@
+export function PlusIcon() {
+  return (
+    <svg
+      width="15"
+      height="16"
+      viewBox="0 0 15 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.20703 15.0703L7.20703 0.928178"
+        stroke="black"
+        strokeWidth="2"
+      />
+      <path
+        d="M0.135963 7.99924L14.2781 7.99925"
+        stroke="black"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
