04318c4184bcb233f5600b68876eccf8277f68ad
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2022-07-14 17:20:28 -0700

    Upgrade to 0.1.7

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021 napari Community"
 author = "napari Community"
 
 
-release = '0.1.6'
+release = '0.1.7'
 version = release.replace("dev0", "")
 
 # -- General configuration ---------------------------------------------------
