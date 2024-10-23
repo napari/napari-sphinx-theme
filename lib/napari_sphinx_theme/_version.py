5f65ea84374027859db27c67494b92dbf8f38c59
DragaDoncila <DragaDoncila@users.noreply.github.com>
2024-10-23 00:09:17 +0000

    deploy: 691992d47de3197fc35c01727082cb04b1cc8367

--- lib/napari_sphinx_theme/_version.py
+++ lib/napari_sphinx_theme/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.4.1.dev4+g2aa9fa3'
-__version_tuple__ = version_tuple = (0, 4, 1, 'dev4', 'g2aa9fa3')
+__version__ = version = '0.5.1.dev1+g691992d'
+__version_tuple__ = version_tuple = (0, 5, 1, 'dev1', 'g691992d')
