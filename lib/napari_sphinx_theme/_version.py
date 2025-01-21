930cee6c4da301f2a245648150a24218cf06982d
melissawm <melissawm@users.noreply.github.com>
2025-01-21 20:14:07 +0000

    deploy: 7e810b569a50479f1c7af543f45c24a3482a2247

--- lib/napari_sphinx_theme/_version.py
+++ lib/napari_sphinx_theme/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.5.1.dev1+g691992d'
-__version_tuple__ = version_tuple = (0, 5, 1, 'dev1', 'g691992d')
+__version__ = version = '0.5.1.dev2+g7e810b5'
+__version_tuple__ = version_tuple = (0, 5, 1, 'dev2', 'g7e810b5')
