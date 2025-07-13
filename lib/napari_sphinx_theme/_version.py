aef6ce92bd6ceb0fef0a40caa7332f553fcb08ce
TimMonko <TimMonko@users.noreply.github.com>
2025-07-13 15:46:33 +0000

    deploy: b32e5e0cebe2cee1893af78e56596c44cfc7293f

--- lib/napari_sphinx_theme/_version.py
+++ lib/napari_sphinx_theme/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.6.1.dev4+gecd6783'
-__version_tuple__ = version_tuple = (0, 6, 1, 'dev4', 'gecd6783')
+__version__ = version = '0.7.1.dev1+gb32e5e0'
+__version_tuple__ = version_tuple = (0, 7, 1, 'dev1', 'gb32e5e0')
