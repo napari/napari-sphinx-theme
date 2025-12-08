c56e2b1145382c3980c875a07a1e688812389c38
melissawm <melissawm@users.noreply.github.com>
2025-12-08 15:54:07 +0000

    deploy: e5ead156a28c3415e5a1c57cdacb5f2ae8c4e691

--- lib/napari_sphinx_theme/_version.py
+++ lib/napari_sphinx_theme/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.0.2.dev3+g80eec2d5b'
-__version_tuple__ = version_tuple = (1, 0, 2, 'dev3', 'g80eec2d5b')
+__version__ = version = '1.0.2.dev4+ge5ead156a'
+__version_tuple__ = version_tuple = (1, 0, 2, 'dev4', 'ge5ead156a')
 
-__commit_id__ = commit_id = 'g80eec2d5b'
+__commit_id__ = commit_id = 'ge5ead156a'
