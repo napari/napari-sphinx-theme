715855ed11a92dc6a4f82af847b294323b3d6fea
TimMonko <TimMonko@users.noreply.github.com>
2026-04-09 22:52:09 +0000

    deploy: 227ba4467cc9ec9ac5c5e6e5cd674b2daab752cd

--- lib/napari_sphinx_theme/_version.py
+++ lib/napari_sphinx_theme/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '1.0.2.dev9+g01fa23b78'
-__version_tuple__ = version_tuple = (1, 0, 2, 'dev9', 'g01fa23b78')
+__version__ = version = '1.0.2.dev10+g227ba4467'
+__version_tuple__ = version_tuple = (1, 0, 2, 'dev10', 'g227ba4467')
 
-__commit_id__ = commit_id = 'g01fa23b78'
+__commit_id__ = commit_id = 'g227ba4467'
